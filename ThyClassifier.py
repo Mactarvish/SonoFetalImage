47fae4ba3265620df4e6e7beff2680e9bccdc202
hdl2 <451143003@qq.com>
2018-01-27 18:23:50 +0800

    Bug: can not train threshold either print threshold's grad.

--- ThyClassifier.py
+++ ThyClassifier.py
@@ -37,9 +37,6 @@ label_map = {"hashimoto_thyroiditis1": 0, "hyperthyreosis1": 1, "normal1": 2, "p
 
 connection = sqlite3.connect("ThyDataset")
 cu = connection.cursor()
-# cu.execute("select * from Train where id=2")
-# a = cu.fetchall()[0]
-# print(a)
 
 torch.manual_seed(123)
 torch.cuda.manual_seed(222)
@@ -74,16 +71,6 @@ def image2modelinput(file_name, model_input_size=None):
     #print(input)
     return input
 
-class ResizeImage(object):
-    """
-    Input an numpy array or a PIL image and return a PIL image with given size "new_size", keeping num of channels unchanged.
-    """
-    def __init__(self, new_size):
-        self.new_size = new_size
-    def __call__(self, image):
-        image = cv2.resize(np.asarray(image), self.new_size)
-        return Image.fromarray(image)
-
 class SizeCoorTransform(object):
     def __init__(self, new_size):
         self.n_size = new_size
@@ -109,84 +96,6 @@ class SizeCoorTransform(object):
                 n_coordinates.append(n_coordinate)
 
         return image, n_coordinates
-
-class BrainSliceDataset(Dataset):
-    # inl: image and labels
-    def __init__(self, train=True, image_transform=None, pre_transform=None):
-        super(BrainSliceDataset, self).__init__()
-        self.train = train
-        self.image_transform = image_transform
-        self.pre_transform = pre_transform
-        self.point_labels = self.get_point_labels()
-        self.categories = self.get_category()
-        self.category_transofm = {"circle": 0, "line": 1, "other": 1}
-        #print(self.labels)
-
-    def __getitem__(self, item):
-        if self.train == True:
-            image = Image.open("/home/hdl2/Desktop/SonoDataset/OriginalImages/%d.jpg" % (item)).convert('L')
-            label = self.category_transofm[self.categories[item]]
-        else:
-            image = Image.open("/home/hdl2/Desktop/SonoDataset/OriginalImages/%d.jpg" % (item + 700)).convert('L')
-            label = self.category_transofm[self.categories[item + 700]]
-
-        if self.image_transform is not None:
-            image = self.image_transform(image)
-
-        return image, label
-
-    def __len__(self):
-        # except .git
-        if self.train == True:
-            return 700
-        else:
-            return 101
-        #return len(os.listdir("/home/hdl2/Desktop/SonoDataset/OriginalImages/")) - 1
-
-    def str2coordinate(self, str):
-        '''
-        :param str: like '(231,55)'
-        :return: str to tuple
-        :example: '(231,55)' -> tuple (231, 55)
-        '''
-        nums = str[1:-1].split(',')
-        #print(nums)
-        return (int(nums[0]), int(nums[1]))
-
-    def get_point_labels(self):
-        f = open("RectLabels.txt")
-        labels = {}
-        lines = f.readlines()
-        for line in lines:
-            # print(line)
-            elements = line.split(' ')
-            coordinates = []
-            index = None
-            for e in elements:
-                #print('e:', e)
-                if '.jpg' in e:
-                    index = int(e[: -4])
-                else:
-                    #bad condition, need repairing later
-                    if e == '\n':
-                        #print('happens')
-                        continue
-                    coordinates.append(self.str2coordinate(e))
-            labels[index] = coordinates
-        return labels
-
-    def get_category(self):
-        f = open("/home/hdl2/Desktop/SonoDataset/Labels/classify.txt")
-        labels = {}
-        lines = f.readlines()
-        for line in lines:
-            print(line)
-            number = int(line.split('.')[0])
-            category = line.split(' ')[-1][:-1]
-            labels[number] = category
-        print(labels)
-        return labels
-
 
 class ThyDataset(Dataset):
     def __init__(self, train=True, image_transform=None, pre_transform=None):
@@ -247,53 +156,8 @@ class ThyDataset(Dataset):
         else:
             return 300
 
-    def str2coordinate(self, str):
-        '''
-        :param str: like '(231,55)'
-        :return: str to tuple
-        :example: '(231,55)' -> tuple (231, 55)
-        '''
-        nums = str[1:-1].split(',')
-        #print(nums)
-        return (int(nums[0]), int(nums[1]))
-
-    def get_point_labels(self):
-        f = open("RectLabels.txt")
-        labels = {}
-        lines = f.readlines()
-        for line in lines:
-            # print(line)
-            elements = line.split(' ')
-            coordinates = []
-            index = None
-            for e in elements:
-                #print('e:', e)
-                if '.jpg' in e:
-                    index = int(e[: -4])
-                else:
-                    #bad condition, need repairing later
-                    if e == '\n':
-                        #print('happens')
-                        continue
-                    coordinates.append(self.str2coordinate(e))
-            labels[index] = coordinates
-        return labels
-
-    def get_category(self):
-        f = open("/home/hdl2/Desktop/SonoDataset/Labels/classify.txt")
-        labels = {}
-        lines = f.readlines()
-        for line in lines:
-            print(line)
-            number = int(line.split('.')[0])
-            category = line.split(' ')[-1][:-1]
-            labels[number] = category
-        print(labels)
-        return labels
-
-
 transformer = transforms.Compose([
-    ResizeImage((255, 255)),
+    mu.ResizeImage((255, 255)),
     transforms.ToTensor(), # range [0, 255] -> [0.0,1.0]
     transforms.Normalize(mean=[0.333, 0.0586, 0.023], std=[0.265, 0.138, 0.0224])
     ]
@@ -305,19 +169,118 @@ val_loader   = DataLoader(ThyDataset(train=False, image_transform=transformer, p
 dataloaders = {"train": train_loader, "val": val_loader}
 dataset_sizes = {"train": len(ThyDataset(train=True)), "val": len(ThyDataset(train=False))}
 
-model_ft = models.resnet18(pretrained=True)
+thresholds = []
+outs = []
 
+class MyResNet(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000):
+        self.inplanes = 64
+        super(MyResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+
+class threshold_BC(models.resnet.BasicBlock):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(threshold_BC, self).__init__(inplanes, planes, stride=stride, downsample=downsample)
+        self.threshold = Variable(torch.Tensor([5.032]), requires_grad=True).cuda()
+        thresholds.append(self.threshold)
+
+    def forward(self, x):
+        print(self.threshold)
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+        out = out + residual * self.threshold
+        out = self.relu(out)
+        outs.append(out)
+
+        return out
+
+def resnet_th(pretrained=False, **kwargs):
+    """Constructs a ResNet model with threshold.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = MyResNet(threshold_BC, [2, 2, 2, 2], **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['resnet18']))
+    return model
+
+# model_ft = models.resnet18(pretrained=False)
+model_ft = resnet_th(pretrained=False)
 num_ftrs = model_ft.fc.in_features
 model_ft.fc = nn.Linear(num_ftrs, 6)
 
 if use_gpu:
     model_ft = model_ft.cuda()
-
 criterion = nn.CrossEntropyLoss()
-
 # Observe that all parameters are being optimized
 optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
-
 # Decay LR by a factor of 0.1 every 7 epochs
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
@@ -365,6 +328,12 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=10):
                 # backward + optimize only if in training phase
                 if phase == 'train':
                     loss.backward()
+
+                    for threshold in thresholds:
+                        print(threshold.grad)
+                    for out in outs:
+                        print(out.grad)
+
                     optimizer.step()
 
                 # statistics
@@ -401,98 +370,3 @@ model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
 
 # model_ft.load_state_dict(torch.load("Thynet.pkl"))
 # print(torch.max(model_ft(image2modelinput("ThyImage/hyperthyreosis1_251701.jpg", (255, 255))), 1)[1])
-
-
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=7)
-        self.conv3 = nn.Conv2d(20, 40, kernel_size=5)
-        self.conv3_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(23040, 50)
-        self.fc2 = nn.Linear(50, 2)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2)) # 10 * 110 * 110
-        x = F.relu(F.max_pool2d(self.conv2(x), 2)) # 20 * 52 * 52
-        x = F.relu(F.max_pool2d(self.conv3_drop(self.conv3(x)), 2)) # 40 * 24 * 24
-        x = x.view(-1, 23040)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x)
-model = Net()
-model.cuda()
-
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.7)
-
-def train(epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        print("target:", target)
-        print(data.shape, type(data))
-        data, target = Variable(data.cuda()), Variable(target.cuda())
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        print("epoch: %d %d/%d (%0.0f%%)\tLoss: %0.6f" % (epoch, epoch * len(data), len(train_loader.dataset), 100. * batch_idx / len(train_loader), loss.data[0]))
-
-good_prediction_count = 0
-
-def validation():
-    model.eval()
-    test_loss = 0
-    correct = 0
-    for batch_idx, (data, target) in enumerate(val_loader):
-        data, target = Variable(data.cuda()), Variable(target.cuda())
-        output = model(data)
-        #F.mse_loss()
-        test_loss += F.nll_loss(output, target, size_average=False).data[0]
-        prediction = output.data.max(1, keepdim=True)[1]
-        correct += prediction.eq(target.data.view_as(prediction)).cpu().sum()
-
-    test_loss /= len(val_loader.dataset)
-    print("\nTest set: Average loss: %.4f, Accuracy: %d/%d (%.0f%%)\n" %(test_loss, correct, len(val_loader.dataset), 100. * correct / len(val_loader.dataset)))
-    if correct >= 95:
-        global good_prediction_count
-        good_prediction_count += 1
-        if good_prediction_count == 3:
-            torch.save(model.state_dict(), 'weights.pkl')
-            #print("training is over!")
-            assert 0, "training is over!"
-    else:
-        good_prediction_count = 0
-
-    time.sleep(1)
-
-def test(n_image):
-    model.load_state_dict(torch.load('weights.pkl'))
-    model.eval()
-    file_name = "/home/hdl2/Desktop/SonoDataset/OriginalImages/%d.jpg" % (n_image)
-    data = image2modelinput(file_name)
-    output = model(data)
-    prediction = output.data.max(1, keepdim=True)[1]
-    prediction = prediction.cpu().numpy()[0][0]
-    couple = {0: "circle", 1: "other"}
-    print(couple[int(prediction)])
-
-circle_index = 0
-def classify(): # 0~10043
-    model.load_state_dict(torch.load('weights.pkl'))
-    model.eval()
-    for i in range(10044):
-        file_name = "/home/hdl2/Desktop/SonoDataset/OriginalImages/%d.jpg" % (i)
-        data = image2modelinput(file_name)
-        output = model(data)
-        prediction = output.data.max(1, keepdim=True)[1]
-        prediction = prediction.cpu().numpy()[0][0]
-        if prediction == 0:
-            print(i)
-            global circle_index
-            shutil.copyfile(file_name, "/home/hdl2/Desktop/SonoDataset/Circles/" + "%d.jpg" % (circle_index))
-            circle_index += 1
-
-
