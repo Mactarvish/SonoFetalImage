ddd48f504cc32f94515378651f68705b40c4612b
hdl2 <451143003@qq.com>
2018-02-28 21:44:33 +0800

    mixup v1, select lam from list manually.

--- ThyClassifier.py
+++ ThyClassifier.py
@@ -195,6 +195,13 @@ val_loader = DataLoader(ThyDataset(train=False, image_transform=transformer, pre
 dataloaders = {"train": train_loader, "val": val_loader}
 dataset_sizes = {"train": len(ThyDataset(train=True)), "val": len(ThyDataset(train=False))}
 
+# for (x1, y1), (x2, y2) in zip(subset_loader1, subset_loader2):
+#     lam = numpy.random.beta(0.2, 0.2)
+#     x = Variable(lam * x1 + (1. - lam) * x2)
+#     y = Variable(lam * y1 + (1. - lam) * y2)
+#     optimizer.zero_grad()
+#     loss(net(x), y).backward()
+#     optimizer.step()
 
 class ThresholdBlock(models.resnet.BasicBlock):
     expansion = 1
@@ -236,12 +243,18 @@ model_ft = resnet_th(pretrained=True)
 #model_ft = models.resnet18(pretrained=False)
 
 num_ftrs = model_ft.fc.in_features
-model_ft.fc = nn.Linear(num_ftrs, 6)
-
+model_ft.fc = nn.Sequential(nn.Linear(num_ftrs, 6), nn.Softmax())
+# self.fc = nn.Sequential(
+#     nn.Linear(16 * 5 * 5, 120),
+#     nn.ReLU(),
+#     nn.Linear(120, 84),
+#     nn.ReLU(),
+#     nn.Linear(84, 10)
+# )
 if use_gpu:
     model_ft = model_ft.cuda()
 
-criterion = nn.CrossEntropyLoss()
+criterion = nn.MSELoss()
 
 # Observe that all parameters are being optimized
 optimizer_ft = optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
@@ -259,56 +272,144 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=10):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
 
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                scheduler.step()
-                model.train(True)  # Set model to training mode
-            else:
-                model.train(False)  # Set model to evaluate mode
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            # Iterate over data.
-            for data in dataloaders[phase]:
+        # train step
+        scheduler.step()
+        model.train(True)  # Set model to training mode
+        running_loss = 0.0
+        running_corrects = 0
+        # Iterate over data.
+        for lam in [0, 0.2, 0.8, 1]:
+            # for data in train_loader:
+            # for i in range(len(train_loader) // 2):
+            for (x1, y1), (x2, y2) in zip(subset_loader1, subset_loader2):
+                # x1 is torch.FloatTensor, y1 is torch.LongTensor, transform to float for mul
+                y1 = y1.float()
+                y2 = y2.float()
                 # get the inputs
-                inputs, labels = data
+                inputs = lam * x1 + (1. - lam) * x2
+                labels = lam * y1 + (1. - lam) * y2
 
                 # wrap them in Variable
-                if use_gpu:
-                    inputs = Variable(inputs.cuda())
-                    labels = Variable(labels.cuda())
-                else:
-                    inputs, labels = Variable(inputs), Variable(labels)
+                inputs = Variable(inputs.cuda())
+                labels = Variable(labels.cuda())
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
-
                 # forward
                 outputs = model(inputs)
-                _, preds = torch.max(outputs.data, 1)
-                loss = criterion(outputs, labels)
+                # _, preds = torch.max(outputs, 1)
+                # preds = preds.float()
+                # loss = criterion(preds, labels)
+
+                labels = to_categorical(labels.data.cpu().numpy(), 6)
+                labels = Variable(torch.from_numpy(labels)).float().cuda()
+                # output:[torch.cuda.FloatTensor of size 6x6 (GPU 0)] labels:[torch.cuda.FloatTensor of size 6x6 (GPU 0)]
+                # loss:[torch.cuda.FloatTensor of size 1 (GPU 0)]
+                loss = nn.MSELoss()(outputs, labels)
 
                 # backward + optimize only if in training phase
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
+                loss.backward()
+                optimizer.step()
 
                 # statistics
                 running_loss += loss.data[0]
-                running_corrects += torch.sum(preds == labels.data)
+                # running_corrects += torch.sum(preds == labels.data)
 
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects / dataset_sizes[phase]
+        epoch_loss = running_loss / (dataset_sizes['train'] * 1.5)
+        # epoch_acc = running_corrects / (dataset_sizes['train'] * 1.5)
 
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
+        print('train Loss: {:.4f}'.format(
+            epoch_loss))
 
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = model.state_dict()
+        # validation step
+        model.train(False)
+        running_loss = 0.0
+        running_corrects = 0
+        # Iterate over data.
+        for data in val_loader:
+            # get the inputs
+            inputs, labels = data
+
+            # wrap them in Variable
+            if use_gpu:
+                inputs = Variable(inputs.cuda())
+                labels = Variable(labels.cuda())
+            else:
+                inputs, labels = Variable(inputs), Variable(labels)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward
+            outputs = model(inputs)
+            _, preds = torch.max(outputs.data, 1)
+            loss = nn.CrossEntropyLoss()(outputs, labels)
+            # loss = criterion(outputs, labels)
+
+            # statistics
+            running_loss += loss.data[0]
+            running_corrects += torch.sum(preds == labels.data)
+
+        epoch_loss = running_loss / dataset_sizes['val']
+        epoch_acc = running_corrects / dataset_sizes['val']
+
+        print('val Loss: {:.4f} Acc: {:.4f}'.format(
+            epoch_loss, epoch_acc))
+
+        # deep copy the model
+        if epoch_acc > best_acc:
+            best_acc = epoch_acc
+            best_model_wts = model.state_dict()
+
+        # # Each epoch has a training and validation phase
+        # for phase in ['train', 'val']:
+        #     if phase == 'train':
+        #         scheduler.step()
+        #         model.train(True)  # Set model to training mode
+        #     else:
+        #         model.train(False)  # Set model to evaluate mode
+        #
+        #     running_loss = 0.0
+        #     running_corrects = 0
+        #     # Iterate over data.
+        #     for data in dataloaders[phase]:
+        #         # get the inputs
+        #         inputs, labels = data
+        #
+        #         # wrap them in Variable
+        #         if use_gpu:
+        #             inputs = Variable(inputs.cuda())
+        #             labels = Variable(labels.cuda())
+        #         else:
+        #             inputs, labels = Variable(inputs), Variable(labels)
+        #
+        #         # zero the parameter gradients
+        #         optimizer.zero_grad()
+        #
+        #         # forward
+        #         outputs = model(inputs)
+        #         _, preds = torch.max(outputs.data, 1)
+        #         loss = criterion(outputs, labels)
+        #
+        #         # backward + optimize only if in training phase
+        #         if phase == 'train':
+        #             loss.backward()
+        #             optimizer.step()
+        #
+        #         # statistics
+        #         running_loss += loss.data[0]
+        #         running_corrects += torch.sum(preds == labels.data)
+        #
+        #     epoch_loss = running_loss / dataset_sizes[phase]
+        #     epoch_acc = running_corrects / dataset_sizes[phase]
+        #
+        #     print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+        #         phase, epoch_loss, epoch_acc))
+        #
+        #     # deep copy the model
+        #     if phase == 'val' and epoch_acc > best_acc:
+        #         best_acc = epoch_acc
+        #         best_model_wts = model.state_dict()
 
         print()
 
